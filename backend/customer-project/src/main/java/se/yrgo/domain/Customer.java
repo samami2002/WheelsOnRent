2f550d1249f8176190beeac747f73923d3d83860
Saman Amirrahmani <samami2002@skola.goteborg.se>
2023-12-18 20:34:29 +0100

    Enhance Customer microservice: add Address entity, update Customer class with OneToOne mapping, and implement API endpoint to add address to a customer

--- backend/customer-project/src/main/java/se/yrgo/domain/Customer.java
+++ backend/customer-project/src/main/java/se/yrgo/domain/Customer.java
@@ -1,9 +1,6 @@
 package se.yrgo.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Customer {
@@ -12,6 +9,12 @@ public class Customer {
     private Long id;
     private String name;
     private String nationalIdentificationNumber;
+    private String email;
+    private String drivingLicenseNumber;
+    private String phoneNumber;
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private Address address;
 
     public Long getId() {
         return id;
@@ -36,5 +39,33 @@ public class Customer {
         this.nationalIdentificationNumber = nationalIdentificationNumber;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDrivingLicenseNumber() {
+        return drivingLicenseNumber;
+    }
+
+    public void setDrivingLicenseNumber(String drivingLicenseNumber) {
+        this.drivingLicenseNumber = drivingLicenseNumber;
+    }
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
