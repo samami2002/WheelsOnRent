e810a38124e8838b5d58658b3effe3c2bfeea430
Saman Amirrahmani <samami2002@skola.goteborg.se>
2023-12-19 17:13:32 +0100

    Implement redirection to confirmation page after successful booking

--- frontend/wheels-on-rent-app/src/App.js
+++ frontend/wheels-on-rent-app/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './components/Home';
 import Booking from './components/Booking';
 import Navigation from './components/Navigation';
 import CarManagement from './components/CarManagement';
+import BookingConfirmation from './components/BookingConfirmation';
 
 function App() {
     return (
         <Router>
-            <Navigation />
+            <Navigation/>
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/booking" element={<Booking />} />
-                <Route path="/car-management" element={<CarManagement />} />
+                <Route path="/" element={<Home/>}/>
+                <Route path="/booking" element={<Booking/>}/>
+                <Route path="/booking-confirmation" element={<BookingConfirmation/>}/>
+                <Route path="/car-management" element={<CarManagement/>}/>
             </Routes>
         </Router>
     );
