ba969e03b5b541c659c9fb0d745f369e2428c023
Saman Amirrahmani <samami2002@skola.goteborg.se>
2023-12-19 17:53:53 +0100

    Add customer search endpoint in CustomerController and corresponding service method to find customer by national identification number

--- backend/customer-project/src/main/java/se/yrgo/data/CustomerRepository.java
+++ backend/customer-project/src/main/java/se/yrgo/data/CustomerRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.yrgo.domain.Customer;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByNationalIdentificationNumber(String nationalIdentificationNumber);
 }
